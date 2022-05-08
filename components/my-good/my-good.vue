5048c510ec3b589421a8d16dd88293034d588cc0
losca <783840358@qq.com>
2022-05-08 19:18:37 +0800

    完成了商品列表的开发

---
+++ components/my-good/my-good.vue
@@ -0,0 +1,72 @@
+<template>
+	<view class="goods-item">
+		<!-- 商品左侧图片区域 -->
+		<view class="goods-item-left">
+			<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+		</view> <!-- 商品右侧信息区域 -->
+		<view class="goods-item-right">
+			<!-- 商品标题 -->
+			<view class="goods-name">{{goods.goods_name}}</view>
+			<view class="goods-info-box">
+				<!-- 商品价格 -->
+				<view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-good",
+		props:{
+			goods:{
+				type:Object,
+				default:{}
+			}
+		},
+		data() {
+			return {
+				defaultPic:'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			};
+		},
+		filters:{
+			// 把数字处理为带两位小数点的数字 
+			tofixed(num) { return Number(num).toFixed(2) }
+		},
+	}
+</script>
+
+<style lang="scss">
+.goods-list{
+	display: flex;
+	flex-direction: column;
+	padding: 5 5px;
+	.goods-item{
+		display: flex;
+		border-bottom: 1px solid #efefef;
+		.goods-item-left{
+			.goods-pic{
+				height: 100px;
+				width: 100px;
+				display: block;
+			}
+		}
+		.goods-item-right{
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			margin-left: 5px;
+			.goods-name{
+				font-size: 13px;
+			}
+			.goods-info-box{
+				
+			}
+			.goods-price{
+				font-size: 16px;
+				color: #c00000;
+			}
+		}
+	}
+}
+</style>
--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		goodsList
+		<view class="goods-list">
+			<view v-for="(goods, i) in goodList" :key="i" @click="gotoGoodsDetail(goods)">
+				<my-good :goods="goods"></my-good>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +12,59 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj: {
+					// 查询关键词 
+					query: '',
+					// 商品分类Id 
+					cid: '',
+					// 页码值 
+					pagenum: 1,
+					// 每页显示多少条数据 
+					pagesize: 10
+				},
+				goodList: [],
+				total: 0,
+				// 节流阀
+				isloading:false
 			};
+		},
+		
+		onLoad(obj) {
+			this.queryObj.cid = obj.cid || ''
+			this.queryObj.query = obj.query || ''
+			this.getGoodList();
+		},
+		methods: {
+			async getGoodList(cb) {
+				// 开启节流阀
+				this.isloading = true;
+				const {
+					data: res
+				} = await uni.$http.get("api/public/v1/goods/search", this.queryObj);
+				// 请求成功关闭节流阀
+				this.isloading = false;
+				cb&&cb();
+				if (res.meta.status != 200) return uni.$showMsg()
+				this.goodList = [...this.goodList,...res.message.goods]
+				this.total = res.message.total
+			},
+			gotoGoodsDetail(goods){
+				uni.navigateTo({
+					url:"/subpkg/goods_detail/goods_detail?goods_id=" + goods.goods_id
+				})
+			}
+		},
+		onReachBottom() {
+			if(this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg("数据加载完毕！")
+			if(this.isloading) return
+			this.queryObj.pagenum++;
+			this.getGoodList();
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1;
+			this.total = 0;
+			this.goodList = [];
+			this.getGoodList(()=> uni.stopPullDownRefresh()) 
 		}
 	}
 </script>
